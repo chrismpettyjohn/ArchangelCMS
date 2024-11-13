3894cd16862634b5457bc64ff089e66c3339a257
chrismpettyjohn <chrismpettyjohn@gmail.com>
2024-11-13 01:08:28 -0500

    feat: disable excess api services

--- apps/imagine-api/src/imagine.module.ts
+++ apps/imagine-api/src/imagine.module.ts
@@ -1,24 +1,14 @@
 import { resolve } from 'path';
 import { Module } from '@nestjs/common';
-import { BanModule } from './ban/ban.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-import { RankModule } from './rank/rank.module';
-import { RoomModule } from './room/room.module';
-import { PhotoModule } from './photo/photo.module';
-import { GroupModule } from './group/group.module';
 import { CommonModule } from './common/common.module';
 import { GraphQLJSONObject } from 'graphql-type-json';
-import { ArticleModule } from './article/article.module';
 import { SessionModule } from './session/session.module';
-import { ChatlogModule } from './chatlog/chatlog.module';
-import { DiscordModule } from './discord/discord.module';
 import { DatabaseModule } from './database/database.module';
 import { databaseEntities } from './database/database.const';
-import { FriendshipModule } from './friendship/friendship.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { WordFilterModule } from './word-filter/word-filter.module';
 import {
   IMAGINE_DATABASE_HOST,
   IMAGINE_DATABASE_NAME,
@@ -26,26 +16,6 @@ import {
   IMAGINE_DATABASE_USER,
   IMAGINE_GRAPHQL_PLAYGROUND,
 } from './imagine.constant';
-import { UserBadgeModule } from './user-badge/user-badge.module';
-import { SupportTicketModule } from './support-ticket/support-ticket.module';
-import { ForgotPasswordModule } from './forgot-password/forgot-password.module';
-import { BetaCodeModule } from './beta-code/beta-code.module';
-import { StaffApplicationModule } from './staff-application/staff-application.module';
-import { RadioRequestModule } from './radio-request/radio-request.module';
-import { BugReportModule } from './bug-report/bug-report.module';
-import { FurnitureModule } from './furniture/furniture.module';
-import { FurniturePurchaseLogModule } from './furniture-purchase-log/furniture-purchase-log.module';
-import { FurnitureCommentModule } from './furniture-comment/furniture-comment.module';
-import { FurnitureReactionModule } from './furniture-reaction/furniture-reaction.module';
-import { UserFurnitureModule } from './user-furniture/user-furniture.module';
-import { StorePackageModule } from './store-package/store-package.module';
-import { StoreTransactionModule } from './store-transaction/store-transaction.module';
-import { StoreTransactionLogModule } from './store-transaction-log/store-transaction-log.module';
-import { GangModule } from './gang/gang.module';
-import { CorporationModule } from './corporation/corporation.module';
-import { CorpRoleModule } from './corporation-role/corporation-role.module';
-import { CorporationMemberModule } from './corporation-member/corporation-member.module';
-import { RPStatsModule } from './rp-stats/rp-stats.module';
 
 @Module({
   imports: [
@@ -89,36 +59,6 @@ import { RPStatsModule } from './rp-stats/rp-stats.module';
     DatabaseModule,
     SessionModule,
     UserModule,
-    RankModule,
-    ArticleModule,
-    BanModule,
-    WordFilterModule,
-    ChatlogModule,
-    RoomModule,
-    GroupModule,
-    PhotoModule,
-    DiscordModule,
-    FriendshipModule,
-    UserBadgeModule,
-    SupportTicketModule,
-    ForgotPasswordModule,
-    BetaCodeModule,
-    StaffApplicationModule,
-    RadioRequestModule,
-    BugReportModule,
-    FurnitureModule,
-    FurniturePurchaseLogModule,
-    FurnitureCommentModule,
-    FurnitureReactionModule,
-    UserFurnitureModule,
-    StorePackageModule,
-    StoreTransactionModule,
-    StoreTransactionLogModule,
-    GangModule,
-    CorporationModule,
-    CorpRoleModule,
-    CorporationMemberModule,
-    RPStatsModule,
   ],
 })
 export class ImagineModule { }
